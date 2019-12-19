a3bf694f8aeeb6daf935947305773dbaabcc4d4e
BreadyX <alex.bradatan85@yahoo.it>
2019-12-19 09:55:43 +0100

    Check for __unix__ macro instead of linux in input-h and graphics.h

--- graphics.h
+++ graphics.h
@@ -85,7 +85,7 @@ void apply_food(map_t *map);
 #   if defined(_WIN64) || defined(_WIN32)
 #       include <stdlib.h>
 #       define clrscr() do {system("cls");} while(0);
-#   else
+#   elif defined(__unix__)
 #       define clrscr() do {printf(CLEAR_CSI);} while(0)
 #   endif
 #endif
--- input.h
+++ input.h
@@ -12,7 +12,7 @@
 #       include <conio.h>
 #       define init_term() ;
 #       define reset_term() ;
-#   elif defined(linux)
+#   elif defined(__unix__)
 #       include <stdlib.h>
 #       include <unistd.h>
 #       include <sys/select.h>
