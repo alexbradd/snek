4353a0142ac832737b285a19953f919d72b67828
BreadyX <alex.bradatan85@yahoo.it>
2019-12-22 00:54:50 +0100

    Added C-c signal handling for Linux/OSX

--- input.c
+++ input.c
@@ -9,18 +9,34 @@
 #include <termios.h>
 #include <string.h>
 
+#include <signal.h>
+
 static struct termios orig_termios;
+
+static void handle_sig_int(int sig);
 
 void init_term(void)
 {
     struct termios new_termios;
+    struct sigaction sig;
 
     tcgetattr(STDIN_FILENO, &orig_termios);
     memcpy(&new_termios, &orig_termios, sizeof(struct termios));
 
     cfmakeraw(&new_termios);
+    new_termios.c_iflag &= ~IGNBRK;
+    new_termios.c_iflag |= BRKINT;
+    new_termios.c_lflag |= ISIG;
     tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
+
+    sig.sa_handler = handle_sig_int;
+    sigemptyset(&sig.sa_mask);
+    sig.sa_flags = 0;
+    sigaction(SIGQUIT, &sig, NULL);
 }
+
+static void handle_sig_int(int sig)
+{ reset_term(); exit(0); }
 
 void reset_term(void)
 { tcsetattr(STDIN_FILENO, TCSANOW, &orig_termios); }
